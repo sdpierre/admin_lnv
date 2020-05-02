d82e69f6ba691dbefa0dc2836995c3f3fb8f388e
Stanley Dany Pierre <stanleydanypierre@gmail.com>
2020-05-02 02:34:36 -0400

    Update index.blade.php

--- Modules/Breaking/Resources/views/index.blade.php
+++ Modules/Breaking/Resources/views/index.blade.php
@@ -1,9 +1,57 @@
 @extends('welcome')
 
 @section('content')
-    <h1>Breaking News</h1>
+<h2>A la minute</h2>
+<br>
+<div class="row">
+    <div class="col-sm-4">
+        <div class="panel panel-success" data-collapsed="0">
+            <!-- panel head -->
+            <div class="panel-heading">
+                <div class="panel-title"> Ajouter une brève </div>
+            </div> <!-- panel body -->
+            <div class="panel-body">
 
-    <p>
-        This view is loaded from module: {!! config('breaking.name') !!}
-    </p>
+                <div class="form-group">
+                    <label for="textfield" class="control-label"><strong>Titre</strong></label>
+                    <div class="controls">
+                        <input type="text" class="form-control" name="titre" value="<?php if (isset($titre)) echo $titre; ?>" required>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label for="textfield" class="control-label"><strong>Catégorie</strong></label>
+                    <div class="controls">
+                        <select class="form-control">
+                            <option> </option>
+                        </select>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label for="textfield" class="control-label"><strong>Texte</strong></label>
+                    <div class="controls">
+                        <textarea class="form-control" rows="20"> </textarea>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                <div class="controls">
+                    <button type="submit" class="btn btn-primary">Publier</button>
+                </div>
+                </div>
+
+  
+                
+
+            </div>
+        </div>
+    </div>
+</div>
+
+</div>
+<div class="col-sm-4">
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate excepturi repudiandae ea! Quisquam dolores in, quo, alias beatae eius a suscipit numquam maiores accusantium perspiciatis fugit nisi enim rem ipsa.
+</div>
+</div>
 @stop
